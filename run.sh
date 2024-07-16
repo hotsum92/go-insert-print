f3befd3ae0dad2939ea74f438917f48324605ac0
hotsuma kawashima <hkawashimajob1992@gmail.com>
2024-07-16 20:17:09 +0900

    update

--- run.sh
+++ run.sh
@@ -1,17 +1,8 @@
 #!/bin/bash -eu
 
-#grep -E 'func (\([^\(]+\))? ?[^\(]+\(' main.go
+sed -E -i '/func (\([^\(]+\))? ?[^\(]+\(.*/{p;s/func (\([^\(]+\))? ?([^ \(]+)\(.*/println("DEV:\2")/}' "$@"
 
-sed -E '/func (\([^\(]+\))? ?[^\(]+\(.*/{p;s/func (\([^\(]+\))? ?([^ \(]+)\(.*/println("DEV:\2")/}' main.go > main.go.tmp
-
-sed -E '/DEV:/' main.go > main.go.tmp
-
-awk '/DEV:/ {print FILENAME ":" FNR ":" $0}' ./main.go.tmp
-awk '{print FILENAME ":" FNR ":" $0}' main.go
-
-sed '6s/DEV:/TEST:/' ./main.go.tmp
-
-for s in $(git grep 'DEV:' ./main.go.tmp); do
+for s in $(git grep 'DEV:' "$@"); do
   path=$(echo $s | cut -d: -f1)
   line=$(echo $s | cut -d: -f2)
 
